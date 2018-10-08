15b85bcf45914efe0c22b99f9381bef1f50aed5f
Svildr <svildr@gmail.com>
2018-10-08 13:50:41 -0400

    Update RealRandom.cpp

--- RealRandom.cpp
+++ RealRandom.cpp
@@ -5,7 +5,7 @@
 int getMilliCount() {
 	timeb tb;
 	ftime(&tb);
-	int nCount = tb.millitm + (tb.time & 0xfffff) * 1000;
+	int nCount = tb.millitm + (tb.time & 0xfffff); // * 1000
 	return nCount;
 }
 
